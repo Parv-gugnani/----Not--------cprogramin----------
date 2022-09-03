ed0ce1c87d5f69f49bfb6e295a84c5ea81b81cdc
Parv-gugnani <9765.parv@gmail.com>
2022-09-03 10:24:14 +0530

    update

---
+++ arr/1.c
@@ -0,0 +1,17 @@
+// input and output
+
+#include <stdio.h>
+int main() {
+  int values[5];
+
+  printf("Enter the 5 integer: ");
+
+  for (int i = 0; i < 5; ++i) {
+    scanf("%d", &values[i]);
+  }
+  printf("Displaying integers: ");
+
+  for (int i = 0; i < 5; ++i) {
+    printf("%d  ", values[i]);
+  }
+}
